c25398c8a4212f94e37145e8c92ac2c6d56360e3
zigster64 <steve@raffmail.net>
2021-01-26 15:08:19 +1000

    Make the sqlite db a bit more fancy

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,24 @@
 const std = @import("std");
 const sqlite = @import("sqlite");
+const string = []const u8;
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+var allocator = &gpa.allocator;
+
+const Employee = struct {
+    id: [128:0]u8,
+    name: [128:0]u8,
+    age: usize,
+    salary: usize,
+};
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
-
-    try std.os.unlink("/home/steve/testfile.db");
+    std.os.unlink("./testfile.db") catch {
+        std.log.info("no exsiting DB to delete\n", .{});
+    };
 
     var db: sqlite.Db = undefined;
     try db.init(.{
-        .mode = sqlite.Db.Mode{ .File = "/home/steve/testfile.db" },
+        .mode = sqlite.Db.Mode{ .File = "./testfile.db" },
         .open_flags = .{
             .write = true,
             .create = true,
@@ -16,48 +26,37 @@ pub fn main() anyerror!void {
         .threading_mode = .MultiThread,
     });
 
-    const creator =
-        \\CREATE TABLE employees (id text, name text, age int, salary int)
-    ;
-    var stmt = try db.prepare(creator);
-    try stmt.exec(.{});
-    stmt.deinit();
-    std.log.info("created the employee table ok\n", .{});
+    var create = try db.prepare("CREATE TABLE employees (id text, name text, age int, salary int)");
+    defer create.deinit();
+    try create.exec(.{});
 
-    const addone =
-        \\INSERT INTO employees (id, name, age, salary) VALUES ('ABC','Mr Smith', 33, 12000);
-    ;
-    var stmtone = try db.prepare(addone);
-    try stmtone.exec(.{});
-    stmtone.deinit();
-    std.log.info("added the 1st employee\n", .{});
+    var add = try db.prepare("INSERT INTO employees (id, name, age, salary) VALUES (?,?,?,?)");
+    defer add.deinit();
+    add.reset();
+    try add.exec(.{ "ABC", "Mr Smith", 33, 12000 });
+    add.reset();
+    try add.exec(.{ "DEF", "Mr Jones", 32, 11000 });
+    add.reset();
+    try add.exec(.{ "GHI", "Mr Wayne", 31, 14000 });
+    add.reset();
+    try add.exec(.{ "JKL", "Mr Fred", 38, 12000 });
+    add.reset();
+    try add.exec(.{ "MNO", "Mr Smythe", 36, 11000 });
+    add.reset();
+    try add.exec(.{ "PQR", "Mr Tonsil", 30, 11000 });
+    add.reset();
+    try add.exec(.{ "STU", "Mr Fancypants", 22, 8000 });
+    add.reset();
+    try add.exec(.{ "VXY", "Mr Thing", 24, 7000 });
+    add.reset();
+    try add.exec(.{ "ZZZ", "Mr What", 27, 6000 });
 
-    const addtwo =
-        \\INSERT INTO employees (id, name, age, salary) VALUES ('DEF','Mrs Smith', 32, 11000);
-    ;
-    var stmttwo = try db.prepare(addtwo);
-    try stmttwo.exec(.{});
-    stmttwo.deinit();
-    std.log.info("added the 2nd employee\n", .{});
-
-    const query =
-        \\SELECT id, name, age, salary FROM employees
-    ;
-    var stmt2 = try db.prepare(query);
-    const row = try stmt2.one(
-        struct {
-            id: [128:0]u8,
-            name: [128:0]u8,
-            age: usize,
-            salary: usize,
-        },
-        .{},
-        .{},
-    );
-    if (row) |field| {
-        std.log.debug("name: {s}, age: {}, id: {s}, salary: {}", .{ std.mem.spanZ(&field.name), field.age, std.mem.spanZ(&field.id), field.salary });
+    var select = try db.prepare("SELECT id,name,age,salary FROM employees ORDER BY salary DESC LIMIT 5");
+    defer select.deinit();
+    var iter = try select.iterator(Employee, .{});
+    while (try iter.next(.{})) |row| {
+        std.log.info("name: {s}, age: {}, id: {s}, salary: {}", .{ std.mem.spanZ(&row.name), row.age, std.mem.spanZ(&row.id), row.salary });
     }
-    stmt2.deinit();
 }
 
 test "test" {
